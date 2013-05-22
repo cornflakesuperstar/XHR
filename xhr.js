fddefa3b2ff28c3c23fc1e587155b6f23bd460be
Jason Kotchoff <cornflakesuperstar@hotmail.com>
2013-05-22 16:36:08 +1000

    introduced a http GET that will cache the results based on the Last-Modified header instead of ttl

--- xhr.js
+++ xhr.js
@@ -15,6 +15,95 @@ XHR = function(){
 // Public functions
 // ================
 
+// Retrieves @url (string) 
+//
+// The result is cached to disk. Next time that url is requested, if a HEAD request
+// identifies a Last-Modified header that hasn't changed, the cache will be used
+//
+// Inspired by: http://developer.appcelerator.com/question/150199/getresponseheader-not-working-in-onreadystatechange-function
+XHR.prototype.getIfModified = function(url, onSuccess, onError, extraParams, requestType) {
+  function doGetIfModified(url, onSuccess, onError, extraParams, requestType) {
+    var HASHED_URI              = "cache_" + Titanium.Utils.md5HexDigest(url);
+    var HTTP_OK                 = 200;
+    var HTTP_NOT_MODIFIED       = 304;
+    var xhr                     = Titanium.Network.createHTTPClient();
+    var onSuccess               = onSuccess               || function(){};
+    var onError                 = onError                 || function(){};
+    var extraParams             = extraParams             || {};
+    var requestType             = requestType             || "HEAD";  // First time around, only do a HEAD request to see if the resource has changed
+    extraParams.async           = extraParams.async       || true;
+    extraParams.contentType     = extraParams.contentType || "application/json";
+    extraParams.ifModifiedSince = Titanium.App.Properties.getString(HASHED_URI);
+  
+    function handleResponse(e){
+      if (this.status == HTTP_OK || this.status == HTTP_NOT_MODIFIED) {
+        var cachedFile = Ti.Filesystem.getFile(Titanium.Filesystem.applicationDataDirectory, HASHED_URI);
+        var lastModified = this.getResponseHeader("Last-Modified");
+  
+        if (requestType == "HEAD") {
+          var timestampMatches = (lastModified == extraParams.ifModifiedSince); 
+          Ti.API.info("stocklight.log Timestamp match: " + timestampMatches + " (" + lastModified + " / " + extraParams.ifModifiedSince + ")");
+          Ti.API.info("stocklight.log Cache exists: " + cachedFile.exists());
+          if (timestampMatches && cachedFile.exists()) {
+            // return the current cached copy of the resource
+            Ti.API.info("stocklight.log retrieved from cache: " + url);
+            Ti.API.info("stocklight.log cached file: " + cachedFile.getNativePath());
+            var data = cachedFile.read();
+            if(extraParams.contentType.indexOf("json") != -1) {
+              data = JSON.parse(data);
+            }
+            onSuccess(data);
+          } else {
+            // otherwise, go and get it
+            Ti.API.info("stocklight.log sending GET request: " + url);
+            Ti.App.Properties.removeProperty(HASHED_URI);
+            doGetIfModified(url, onSuccess, onError, extraParams, 'GET');
+          }
+        } else if (requestType == "GET") {
+          // store and return the retrieved response
+          Ti.API.info("stocklight.log retrieved: " + url);
+          var data = this.responseText
+          if(cachedFile.write(data)){
+            // https://www.google.com.au/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&cad=rja&ved=0CCsQFjAA&url=https%3A%2F%2Fjira.appcelerator.org%2Fbrowse%2FTIMOB-1658&ei=bB2YUbidD-aNiAfpyoGQCg&usg=AFQjCNHqEw5yKbtwSrbu84bSdsygyW9eng&bvm=bv.46751780,d.aGc
+            Ti.App.Properties.setString(HASHED_URI, lastModified);
+            Ti.API.info('stocklight.log Written: ' + cachedFile.getNativePath() );
+          } else {
+            Ti.API.info('stocklight.log failed to write: ' + cachedFile.getNativePath() );
+          }
+          if(extraParams.contentType.indexOf("json") != -1) {
+            data = JSON.parse(data);
+          }
+          onSuccess(data);
+        }
+      } else {
+        result.status = "error";
+        result.data = e;
+        result.code = xhr.status;
+        onError(result);
+      }
+    }
+  
+    xhr.onload = handleResponse;
+    xhr.onerror = handleResponse;
+
+    // Set any required headers and send the HTTP request
+    Ti.API.info('stocklight.log sending head request to: ' + url );
+    xhr.open(requestType, url, extraParams.async);
+    xhr.setRequestHeader('If-Modified-Since', extraParams.ifModifiedSince);
+    xhr.setRequestHeader('Content-Type', extraParams.contentType);
+    if(extraParams.headers) {
+      for(i in extraParams.headers) {
+        var header = extraParams.headers[i];
+        xhr.setRequestHeader(header.name, header.value);
+      }
+    }
+    xhr.send();
+  }
+  
+  // Schedule a HEAD request
+  doGetIfModified(url, onSuccess, onError, extraParams, requestType);
+}
+
 // GET 
 // @url (string) URL to fetch
 // @onSuccess (function) success callback
